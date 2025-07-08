fc3bdbd4c0470a1b47c7bd4bd7242047dbb175f0
acer51-doctom <110639772+acer51-doctom@users.noreply.github.com>
2025-07-08 11:39:02 +0200

    Added dependencies and added a TUI

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,61 @@
-use std::fs::File;
-use std::io::{Read, Seek, SeekFrom};
-use std::env;
+use std::fs::{File, metadata};
+use std::io::{self, Read, Seek, SeekFrom, Write};
+use std::process::Command;
+use colored::*;
 
+const COMMON_KEY_OFFSET: u64 = 0xE0;
 
 fn main() -> std::io::Result<()> {
-    // get the path from input
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        eprintln!("Usage: {} <path/to/otp_dump.bin", args[0]);
+    // Clear terminal
+    clear_screen();
+
+    // Prompt
+    println!("Where is your OTP path?");
+    println!("You can drag and drop it in Finder / File Explorer.");
+
+    print!("> ");
+    io::stdout().flush().unwrap();
+
+    // Read input
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    // Trim whitespace and possible quotes/apostrophes
+    let path = input.trim().trim_matches('"').trim_matches('\'');
+
+    // Check if file exists
+    if !metadata(path).is_ok() {
+        eprintln!("{}", "ERROR! Path does not exist".red());
         std::process::exit(1);
     }
-    let path = &args[1];
 
-    // open file
+    // Open file and seek to Common Key offset
     let mut file = File::open(path)?;
+    file.seek(SeekFrom::Start(COMMON_KEY_OFFSET))?;
 
-    // get common key offset (to be added)
-    file.seek(SeekFrom::Start(0xE0))?;
-
-    // read the first 16 bytes
-    let mut key = [0xE0u8; 16];
+    // Read 16 bytes (the key)
+    let mut key = [0u8; 16];
     file.read_exact(&mut key)?;
 
-    // print key in hex(adecimal)
-    print!("Your Common Key is: ");
+    // Print key in hex
+    println!("\nCommon Key:");
     for byte in &key {
         print!("{:02X}", byte);
     }
     println!();
 
     Ok(())
+}
+
+fn clear_screen() {
+    if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .args(&["/C", "cls"])
+            .status()
+            .unwrap();
+    } else {
+        Command::new("clear")
+            .status()
+            .unwrap();
+    }
 }
